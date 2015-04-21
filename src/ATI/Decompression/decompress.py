7c55c5074ff5c37d6aabb1e3b797d5323cb1c197
66095021 <66095021@qq.com>
2015-04-21 17:37:59 +0800

    recursive the compression files.

--- src/ATI/Decompression/decompress.py
+++ src/ATI/Decompression/decompress.py
@@ -4,7 +4,7 @@ import tarfile
 import zipfile
 import rarfile
 import sys
-
+import os
 sys.path.append("/opt/ATI")
 from dirutil import * 
 from logger  import * 
@@ -13,6 +13,36 @@ from logger  import *
 #tar = tarfile.open("/root/test.tar.bz2", "r")
 
 #tar.extractall("/tmp/fuck/")
+
+#find all files  in /tmp/decompression/foo/, if it is 
+def recursion_dir(pathname):
+    while True:
+        files=get_files_for_dir(pathname)
+        logger.debug("files in pathname is %s" %(files))
+        for i  in files:
+            if tarfile.is_tarfile(i):
+                tarfile.open(i).extractall(pathname)
+                os.remove(i)
+            if  zipfile.is_zipfile(i):
+                zipfile.ZipFile(i).extractall(pathname)
+                os.remove(i)
+            if  rarfile.is_rarfile(i):
+                logger.debug("%s is a rar,extract it to %s "%(i,pathname))
+                rarfile.RarFile(i).extractall(pathname)
+                os.remove(i)
+                logger.debug("rm -rf "+i)
+        #can we exit now 
+        
+        compression=0
+        for  i  in get_files_for_dir(pathname):
+            if  tarfile.is_tarfile(i) or zipfile.is_zipfile(i) or  rarfile.is_rarfile(i):
+                compression=1
+        #still have compression file, keep while 
+        if compression==1:
+            continue   
+        return
+
+    
 
 
 #this function should uncompression, and return a list contains all decompressed file 
@@ -23,27 +53,22 @@ def uncompression(filename):
             logger.debug("file %s is tar file"%(filename))
             tar = tarfile.open(filename, "r")
             tar.extractall("/tmp/decompression/"+filename.split('/')[-1])
-            info="/tmp/decompression/"+filename.split('/')[-1]
-            logger.debug("untar it to %s" %(info))
+            recursion_dir("/tmp/decompression/"+filename.split('/')[-1])
             ret=get_files_for_dir("/tmp/decompression")    
-            logger.debug(ret)
             return ret
         if  zipfile.is_zipfile(filename):
             zfile = zipfile.ZipFile(filename)
             zfile.extractall("/tmp/decompression/"+filename.split('/')[-1])
-            ret=get_files_for_dir("/tmp/decompression")
-            return ret
+            recursion_dir("/tmp/decompression/"+filename.split('/')[-1])
+            return get_files_for_dir("/tmp/decompression")
 
         if  rarfile.is_rarfile(filename):
             rfile = rarfile.RarFile(filename)
             rfile.extractall("/tmp/decompression/"+filename.split('/')[-1])
 
-            logger.debug("unrar it to /tmp/decompression/filename")
-            ret=get_files_for_dir("/tmp/decompression")
-            for i in ret:
-                logger.debug("the list is %s" %ret)
-                #uncompression(i)
-            return ret
+            logger.debug("unrar it to /tmp/decompression/%s" %(filename.split('/')[-1]))
+            recursion_dir("/tmp/decompression/"+filename.split('/')[-1])
+            return get_files_for_dir("/tmp/decompression")
 
     except:
           return  None
