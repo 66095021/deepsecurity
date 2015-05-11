d0645a31b5b082e1f0e53626566e65f587d31eb2
66095021 <66095021@qq.com>
2015-05-11 23:02:54 +0800

    log can NOT split by space, since file path may contain space. Change a new way, the final solution should be log is xml or json

--- src/ThreatSphere/Behavior/extract.py
+++ src/ThreatSphere/Behavior/extract.py
@@ -54,14 +54,19 @@ pid_list={}
 #
 #it will return dict of a line 
 import time
+from logger import * 
 def get_meta_of_line(line):
     meta={}
     line=line.replace("<system-event ","")
     line=line.replace("/>","")
     line=line.replace(" ","-",1)
-    str_split=line.split()
+    str_split=line.split('"')
+    logger.debug(str_split)
     for i in str_split:
-        meta[i.split("=")[0]]=i.split("=")[1].replace('"','')
+        if len(i) and i[-1]=="=":
+            meta[i.replace("=", "").strip()]=str_split[str_split.index(i)+1]
+        #meta[i.split("=")[0]]=i.split("=")[1].replace('"','')
+    logger.debug(meta)
     tup_birth = time.strptime(meta["time"].split('.')[0], "%d/%m/%Y-%H:%M:%S");
     birth_secds = time.mktime(tup_birth)   
     meta["utc_time"]=birth_secds
@@ -74,10 +79,20 @@ def get_pid_action(line):
     global extract_list
     meta=get_meta_of_line(line)
     pid=meta["processId"]
+    pname=meta["process"]
+    logger.debug ("%s %s %s" %(pname, pid,meta))
     #some situations here,
     # 1) if the process name is contains Captureclient, then we get a) the pid and path in object1/2,  we just set up a dict contains the pid/name, statusinto the extract_list
     # 2) if not, it is a normal process which is monitored,
     #  2a)  we need to put its action into extract_list if the delta time is smaller than 15 ,and there is no terminate action 
     #  2b) if we met the terminate action from Captureclient, we need to change the status to done. push it into queue 
+    if pname.find("CaptureClient.exe") != -1 and meta["type"] == "process" and meta["action"]=="created":
+       behavior={}
+       behavior["processId"]=meta["object1"]
+       behavior["process"]=meta["object2"]
+       behavior["start_time"]=meta["time"]
+       behavior["start_utc_time"]=meta["utc_time"]
+       
 if __name__ == "__main__":
-    print get_meta_of_line('<system-event time="25/4/2015 2:15:1.543" type="process" processId="612" process="C:\WINDOWS\system32\smss.exe" action="created" object1="2696" object2="C:\WINDOWS\system32\csrss.exe"/>')
+    get_pid_action('<system-event time="25/4/2015 2:15:1.543" type="process" processId="612" process="C:\WINDOWS\system32\smss.exe" action="created" object1="2696" object2="C:\WINDOWS\system32\csrss.exe"/>')
+    get_pid_action('<system-event time="24/4/2015 18:7:51.347" type="process" processId="3224" process="C:\Program Files\Capture\CaptureClient.exe" action="created" object1="4088" object2="C:\WINDOWS\Temp\PUTTY.EXE.exe"/>')
